d1a84afb1eb389e311e30b692f39b1da19c80ca7
Ravi Panchal <81222901+Ravi02205@users.noreply.github.com>
2024-01-28 23:54:01 +0530

    Time: 7 ms (22.68%), Space: 43 MB (5.84%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,8 +1,8 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        int max=Math.max(word1.length(),word2.length());
+        int maxN=Math.max(word1.length(),word2.length());
         String str="";
-        for(int i=0;i<max;i++){
+        for(int i=0;i<maxN;i++){
             if(i<word1.length()){
                str+= word1.charAt(i);
             }
