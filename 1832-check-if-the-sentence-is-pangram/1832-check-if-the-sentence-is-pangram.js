36aea876be5f3f803f55643c60bf0f930e0d93e2
Ravi Panchal <81222901+Ravi02205@users.noreply.github.com>
2024-01-31 23:59:04 +0530

    Time: 49 ms (81.39%), Space: 49.3 MB (12.64%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.js
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.js
@@ -3,12 +3,5 @@
  * @return {boolean}
  */
 var checkIfPangram = function(sentence) {
-    let arr=[...new Set(sentence.split(''))];
-    let chars='abcdefghijklmnopqrstuvwxyz'.split('');
-    for(let i=0;i<chars.length;i++){
-        if(arr.indexOf(chars[i])==-1){
-            return false;
-        }
-    }
-    return true;
+    return new Set(sentence.split("")).size == 26;
 };
