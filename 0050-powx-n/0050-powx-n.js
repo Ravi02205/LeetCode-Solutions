dc431375eb29bd0b38acf34a9234fc9a74c4a789
Ravi Panchal <81222901+Ravi02205@users.noreply.github.com>
2024-02-01 23:45:29 +0530

    Time: 49 ms (80.99%), Space: 49.1 MB (7.02%) - LeetHub

--- 0050-powx-n/0050-powx-n.js
+++ 0050-powx-n/0050-powx-n.js
@@ -4,5 +4,21 @@
  * @return {number}
  */
 var myPow = function(x, n) {
-    return Math.pow(x,n);
+   if (n === 0) {
+    return 1;
+  }
+
+  if (n < 0) {
+    x = 1 / x;
+    n = -n;
+  }
+
+  if (n % 2 === 0) {
+    
+    let halfPower = myPow(x, n / 2);
+    return halfPower * halfPower;
+  }
+  else {
+    return x * myPow(x, n - 1);
+  }  
 };
