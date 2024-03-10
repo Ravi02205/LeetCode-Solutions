3ad4179151605425c765c86ffe98c1f1ac4aa9b7
Ravi Panchal <81222901+Ravi02205@users.noreply.github.com>
2024-03-10 14:41:14 +0530

    Time: 65 ms (41.83%), Space: 52.2 MB (47.71%) - LeetHub

--- 1725-number-of-rectangles-that-can-form-the-largest-square/1725-number-of-rectangles-that-can-form-the-largest-square.js
+++ 1725-number-of-rectangles-that-can-form-the-largest-square/1725-number-of-rectangles-that-can-form-the-largest-square.js
@@ -3,20 +3,17 @@
  * @return {number}
  */
 var countGoodRectangles = function(rectangles) {
-    let arr=[];
-    let max=0;
-    let count=0;
-    for(let i=0;i<rectangles.length;i++){
-        let min=Math.min(rectangles[i][0],rectangles[i][1]);
-        arr.push(min);
-        if(min>max){
-            max=min;
+    let count = 0
+    let max = 0
+    
+    for(let i of rectangles){
+        let side = Math.min(i[0], i[1])
+        if(side > max){
+            max = side
+            count = 1
+        } else if(side == max) {
+            count++
         }
     }
-    for(let i=0;i<arr.length;i++){
-        if(arr[i]==max){
-            count++;
-        }
-    }
-    return count;
+    return count
 };
