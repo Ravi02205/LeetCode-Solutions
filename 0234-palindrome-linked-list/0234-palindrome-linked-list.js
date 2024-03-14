d15acd70c7081a254ab83aea69ae0112fff81a7f
Ravi Panchal <81222901+Ravi02205@users.noreply.github.com>
2024-03-14 22:23:46 +0530

    Time: 136 ms (56.16%), Space: 75.6 MB (55.62%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    if(head == null || head.next == null) return true;
+        var slow = head;
+        var fast = head;
+        while(fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        slow.next = reverseList(slow.next);
+        slow = slow.next;
+        while(slow != null) {
+            if(head.val != slow.val) return false;
+            head = head.next;
+            slow = slow.next;
+        }
+        return true;
+};
+
+var reverseList = function(head) {
+        var pre = null;
+        var next = null;
+        while(head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+};
